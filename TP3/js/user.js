9f59cd51ae533babb5ad088a5b9b118b5d19f42b
Charlotte <charlottepineau98@gmail.com>
2019-03-03 11:32:46 +0100

    Ajout JS inscriptions

--- TP3/js/user.js
+++ TP3/js/user.js
@@ -1,8 +1,8 @@
-import { User } from './models/User.js';
+import {User} from './models/User.js';
 
-function main(){
+function main() {
     console.log(User.getUsers());
-    (User.getUsers().length == 0) ? User.setMocksUser() : "";
+    (User.getUsers().length === 0) ? User.setMocksUser() : "";
     var connexionButton = document.getElementById("submit");
     connexionButton.onclick = connexion;
 }
@@ -14,26 +14,26 @@ function connexion() {
     let username = document.getElementById("username").value;
     let password = document.getElementById("password").value;
 
-    var user;
+    let user;
 
-    UserArray.forEach(function(element){
+    UserArray.forEach(function (element) {
         if (username === element.username) {
             reconnu = true;
-            if (password === element.password){
+            if (password === element.password) {
                 valide = true;
                 user = element;
             }
         }
     });
 
-    if(!reconnu){
+    if (!reconnu) {
         document.getElementById("info").textContent = "Identifiants inconnus";
         document.getElementById("username").value = "";
         document.getElementById("password").value = "";
         return;
     }
 
-    if(!valide){
+    if (!valide) {
         document.getElementById("info").textContent = "Mauvais mot de passe";
         document.getElementById("password").value = "";
         return;
@@ -41,11 +41,58 @@ function connexion() {
 
     document.getElementById("info").textContent = "Vous êtes identifié";
     User.setLoggedUser(user);
-    document.location.href="index.html";
-        
+    document.location.href = "index.html";
+
+}
+
+function mainInscription() {
+    var inscriptionButton = document.getElementById("submit");
+    inscriptionButton.onclick = inscription;
+}
+
+function inscription() {
+    let UserArray = User.getUsers();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let confirm = document.getElementById("password-confirm").value;
+    let reconnu = false;
+    let valide = false;
+
+    UserArray.forEach(function (element) {
+        if (username === element.username) {
+            reconnu = true;
+        }
+    });
+
+    if (password === confirm){
+        valide = true;
+    }
+
+    if (reconnu) {
+        document.getElementById("info").textContent = "Veuillez choisir un autre identifiant";
+        document.getElementById("username").value = "";
+        document.getElementById("password").value = "";
+        document.getElementById("password-confirm").value = "";
+        return;
+    }
+
+    if (!valide){
+        document.getElementById("info").textContent = "Les deux mots de passes sont différents";
+        document.getElementById("password").value = "";
+        return;
+    }
+
+    let inscrit = new User(username, password);
+    User.addUser(inscrit);
+    document.getElementById("info").textContent = "Vous êtes bien inscrit et identifié";
+    User.setLoggedUser(inscrit);
+    document.location.href = "index.html";
 }
 
 
 window.onload = function () {
-    main();
+    if (window.document.title === "Connexion") main();
+    else if (window.document.title === "Inscription") {
+        mainInscription();
+    }
 };
